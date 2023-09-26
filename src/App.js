a09fb93b46fd297ff9f4fab02c8ab9a2c0ebeea3
Ji <Jikang.dev@gmail.com>
2023-09-26 16:38:41 -0400

    created foundation of the application and incorporated individual card components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {"src": "/images/bulbasaur.png"},
+  {"src": "/images/Charmander.png"},
+  {"src": "/images/eevee.png"},
+  {"src": "/images/pikachu.png"},
+  {"src": "/images/psyduck.png"},
+  {"src": "/images/squirtle.png"}
+]
 
 function App() {
+const [cards, setCards] = useState([]);
+const [turns, setTurns] = useState(0)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+      {cards.map(card => (
+        <SingleCard key={card.id} card={card}/>
+      ))}
+      </div>
     </div>
   );
 }
