84fb2d7f378a2c1b440a9f071fcb8921d3d363b7
Ji <Jikang.dev@gmail.com>
2023-09-28 18:38:22 -0400

    rest choices and increase turn functions implemented

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ const handleChoice = (card) => {
   choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
 }
 
+//reset choices & increase turn
+const resetTurn = () => {
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns +1)
+}
+
   return (
     <div className="App">
       <h1>Pokemon Match</h1>
